3d32d2cef00280eca277278796d15f31ae6305cf
Yohannes Abera <yohannesns@gmail.com>
2018-05-28 02:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 
 }
 function deleteFromObjectByKey(object,key){
-  delete recipes[key]
-  return recipes
+return  delete Object[key]
+
 
 }
 function destructivelyDeleteFromObjectByKey(object,key){
