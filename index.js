15f24fca5af77fc7f0be6b58364f1a2f77661f63
Yohannes Abera <yohannesns@gmail.com>
2018-05-28 02:27:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var recipes = {breakfast: 'cereal'}
 
-function updateObjectWithKeyAndValue(object,key,value){
-     Object.assign({},{lunch:'burger'})
-      return recipes
-}
+// function updateObjectWithKeyAndValue(object,key,value){
+//      Object.assign({},{lunch:'burger'})
+//       return recipes
+// }
 function updateObjectWithKeyAndValue(object,key,value){
   return recipes.assign({},object,{[key: value}
-  
+
 }
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 
