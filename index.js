b4aa4bdd24af36c5671c943ccf41e324fd2d2846
Yohannes Abera <yohannesns@gmail.com>
2018-05-28 02:09:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {breakfast: 'cereal'}
+
+function updateObjectWithKeyAndValue(object,key,value){
+     Object.assign({},{lunch:'burger'})
+      return recipes
+}
+function updateObjectWithKeyAndValue(object,key,value){
+  
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  
+}
+function deleteFromObjectByKey(object,key){
+  
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+  
+}
